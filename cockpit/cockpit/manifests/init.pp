4cdd46ca5e41cf3a07cb068d83032a75df269a0d
Greg Perry <greg.perry@encore.tech>
2019-07-10 11:19:38 -0400

    changed static file content method

--- cockpit/cockpit/manifests/init.pp
+++ cockpit/cockpit/manifests/init.pp
@@ -4,15 +4,8 @@ class cockpit {
     ensure => latest,
   }
 
-  $content =  "# This file is managed by puppet
-[WebService]
-LoginTitle=Configured by Puppet
-MaxStartups=5
-AllowUnencrypted=false
-"
-
   file { '/etc/cockpit/cockpit.conf':
-    content => $content,
+    source => '/root/cockpit/cockpit/files/cockpit.conf',
     mode => "0644",
     owner => "root",
     group => "root",
